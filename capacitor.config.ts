345d5d9c1310aec56b17041fcb07ca7f3ad0c553
bounddigital <austin@bounddigital.com>
2023-07-17 13:17:38 -0600

    Adding Facebook Connect package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,13 @@ const config: CapacitorConfig = {
   appId: 'co.cadey.liteapp',
   appName: 'CadeyLite',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    FacebookConnect: {
+      APP_ID: "975686806801415",
+      APP_NAME: "CadeyLite"
+    }
+  }
 };
 
 export default config;
