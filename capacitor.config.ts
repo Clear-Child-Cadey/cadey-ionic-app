fc88eab921c43f833e0d96a41172c237cf737669
bounddigital <austin@bounddigital.com>
2023-07-25 11:27:00 -0600

    Installing Capacitor Splash Screen plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,24 @@ const config: CapacitorConfig = {
   appName: 'CadeyLite',
   webDir: 'dist',
   bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 10000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
