8d7f45e94928698de3b718273e93bab406f4a239
bounddigital <austin@bounddigital.com>
2023-08-28 10:12:58 -0600

    Fixing deeplinks from OneSignal on Android

--- src/api/OneSignal/InitOneSignal.tsx
+++ src/api/OneSignal/InitOneSignal.tsx
@@ -1,7 +1,4 @@
-import { useContext } from 'react';
 import OneSignal from 'onesignal-cordova-plugin';
-import { setExternalUserId } from './SetExternalUserId';
-import { CadeyUserContext } from '../../main';
 
 export function initializeOneSignal(onNotificationOpened: (data: any) => void): void {
     console.log('Initializing OneSignal...');
@@ -13,21 +10,10 @@ export function initializeOneSignal(onNotificationOpened: (data: any) => void): 
 
     OneSignal.setAppId("9e338438-0d42-44e8-b8f4-3ae40f3665e0");
     // Handler for when a user taps on a notification
-    console.log('Setting notificationOpenedHandler...');
     if (window.cordova) {
         OneSignal.setNotificationOpenedHandler(function(jsonData) {
             console.log('notificationOpenedCallback: ', JSON.stringify(jsonData));
             onNotificationOpened(jsonData);
         });
-    }
-
-    const { oneSignalId } = useContext(CadeyUserContext);
-
-    // Check if the app is running in a browser or on a device
-    if (window.cordova) {
-        // Set the external user ID for OneSignal
-        setExternalUserId(oneSignalId.toString());
-    } else {
-        // Don't interact with OneSignal (which relies on Cordova)
     }
 }
