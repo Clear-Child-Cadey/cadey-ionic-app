fd1b6f8d0317c4e5c7086c1c61b7a8ef03259f8c
bounddigital <austin@bounddigital.com>
2023-09-07 12:16:10 -0600

    Packaging production build

--- dist/assets/web-06bc8887.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-6e5a020b.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-f858ba2d.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-43c589bf.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
