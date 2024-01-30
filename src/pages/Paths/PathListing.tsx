c5342bab732dd283f09c85878ea72f4d79430721
bounddigital <austin@bounddigital.com>
2024-01-30 14:07:26 -0700

    Finishing up path listing except for icons and progress circle

--- src/pages/Paths/PathListing.tsx
+++ src/pages/Paths/PathListing.tsx
@@ -1,31 +1,29 @@
 import React, { useContext, useEffect } from 'react';
 import {
-  IonItem,
-  IonLabel,
-  IonCheckbox,
-  IonText,
-  IonRow,
-  IonButton,
-  IonLoading,
-  IonIcon,
+    IonItem,
+    IonLabel,
+    IonText,
+    IonRow,
+    IonLoading,
+    IonIcon,
+    IonHeader,
+    IonToolbar,
+    IonContent,
+    IonPage,
 } from '@ionic/react';
 // Icons
 import { 
     play, 
-    refresh,
     happyOutline,
 } from 'ionicons/icons';
 import './PathListing.css';
 import { useHistory } from 'react-router-dom';
 // Interfaces
-import { Symptom } from '../../components/ConcernsList/ConcernsList';
 import { PathListing, Path } from '../../api/Paths';
 // Contexts
 import { useLoadingState } from '../../context/LoadingStateContext';
 import { CadeyUserContext } from '../../main';
 import ApiUrlContext from '../../context/ApiUrlContext';
-// Modals
-import AgeGroupModal from '../../components/Modals/AgeGroupModal/AgeGroupModal';
 // API
 import { getPathListing } from '../../api/Paths';
 
@@ -64,31 +62,40 @@ const PathListingPage: React.FC = () => {
     };
 
   return (
-    <div className="path-list">
+    <IonPage className="path-listing">
+        <IonContent fullscreen>
+            <IonHeader class="header">
+                <IonToolbar className="header-toolbar">
+                    <h2>Paths</h2>
+                </IonToolbar>
+            </IonHeader>
+            <div className="path-list">
 
-        {/* Show a loading state if necessary */}
-        {isLoading && (
-          <IonLoading isOpen={true} message={'Loading data...'} />
-        )}
+                {/* Show a loading state if necessary */}
+                {isLoading && (
+                <IonLoading isOpen={true} message={'Loading data...'} />
+                )}
 
-        <IonRow>
-            <IonText className="subcopy">Is your child...</IonText>
-        </IonRow>
-        
-        {/* Create a list of rows with an icon on the left, then text, and a play icon on the right for each path in pathListing */}
-        {pathListing && pathListing.paths.map((path: Path) => (
-            <IonItem key={path.id} onClick={() => handlePathSelection(path)}>
-                <IonIcon icon={happyOutline} />
-                <IonLabel>
-                    <h2>{path.pathName}</h2>
-                    <p>{path.numItemsCompleted} of {path.totalItemsInPath} completed</p>
-                </IonLabel>
-                <IonIcon icon={play} />
-            </IonItem>
-        ))}
-
-        {/* Show a button to refresh the page */}
-    </div>
+                <IonRow>
+                    <IonText className="subcopy">Pick up where you left off, or explore new paths</IonText>
+                </IonRow>
+                
+                {/* Create a list of rows with an icon on the left, then text, and a play icon on the right for each path in pathListing */}
+                {pathListing && pathListing.paths.map((path: Path, index) => (
+                    <div className="path" key={path.id} onClick={() => handlePathSelection(path)}>
+                        <IonIcon icon={happyOutline} className='path-icon' />
+                        <IonLabel className='path-name'>
+                            <h2>{path.pathName}</h2>
+                            <p>{path.numItemsCompleted} of {path.totalItemsInPath} completed</p>
+                        </IonLabel>
+                        <div className='play-icon-wrapper'>
+                            <IonIcon icon={play} className='play-icon' />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </IonContent>
+    </IonPage>
   );
 };
 
