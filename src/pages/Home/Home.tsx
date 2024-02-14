b03d6efa496a30b0c95e9f2d51af62a0365ea6d7
Mat Gargano <mgargano@gmail.com>
2024-02-14 14:05:19 -0500

    [MG] baseline, save Home.tsx as is, just code formatted, no other changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,54 +1,59 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './Home.css';
-import { 
-    IonPage, 
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonRow,
-    IonText,
-    IonLoading,
-    IonSearchbar,
-} from '@ionic/react';
-import { SplashScreen } from '@capacitor/splash-screen';
+import React, { useEffect, useState, useRef } from "react";
+import "./Home.css";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRow,
+  IonText,
+  IonLoading,
+  IonSearchbar,
+} from "@ionic/react";
+import { SplashScreen } from "@capacitor/splash-screen";
 // Routing
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 // Contexts
-import { useSpotlight } from '../../context/SpotlightContext';
-import { useModalContext } from '../../context/ModalContext';
-import { useAppPage } from '../../context/AppPageContext';
-import { CadeyUserContext } from '../../main';
-import ApiUrlContext from '../../context/ApiUrlContext';
+import { useSpotlight } from "../../context/SpotlightContext";
+import { useModalContext } from "../../context/ModalContext";
+import { useAppPage } from "../../context/AppPageContext";
+import { CadeyUserContext } from "../../main";
+import ApiUrlContext from "../../context/ApiUrlContext";
 // Components
-import ArticlesListHorizontal from '../../components/Articles/ArticlesListHorizontal';
-import VideoList from '../../components/Videos/VideoList';
-import PopularSymptomsList from '../../components/SymptomsList/PopularSymptomsList';
+import ArticlesListHorizontal from "../../components/Articles/ArticlesListHorizontal";
+import VideoList from "../../components/Videos/VideoList";
+import PopularSymptomsList from "../../components/SymptomsList/PopularSymptomsList";
 // API
-import getHomeData from '../../api/HomeData';
-import { logUserFact } from '../../api/UserFacts';
-import { getQuiz } from '../../api/Quiz';
-import { logErrorToFirestore } from '../../api/Firebase/logErrorToFirestore';
+import getHomeData from "../../api/HomeData";
+import { logUserFact } from "../../api/UserFacts";
+import { getQuiz } from "../../api/Quiz";
+import { logErrorToFirestore } from "../../api/Firebase/logErrorToFirestore";
 // Variables
-import { tracingEnabled } from '../../variables/Logging';
+import { tracingEnabled } from "../../variables/Logging";
 // Firebase
-import { firebasePerf } from '../../api/Firebase/InitializeFirebase';
+import { firebasePerf } from "../../api/Firebase/InitializeFirebase";
 import { trace } from "firebase/performance";
 
-const HomePage: React.FC<{ 
-  currentTab: string, 
-  tutorialStep: number, 
-  setTutorialStep: React.Dispatch<React.SetStateAction<number>>, 
-  vimeoIdFromUrl?: string,
-  articleIdFromUrl?: string,
-}> = ({ currentTab, tutorialStep, setTutorialStep, vimeoIdFromUrl, articleIdFromUrl }) => {
-  
+const HomePage: React.FC<{
+  currentTab: string;
+  tutorialStep: number;
+  setTutorialStep: React.Dispatch<React.SetStateAction<number>>;
+  vimeoIdFromUrl?: string;
+  articleIdFromUrl?: string;
+}> = ({
+  currentTab,
+  tutorialStep,
+  setTutorialStep,
+  vimeoIdFromUrl,
+  articleIdFromUrl,
+}) => {
   const [featuredVideos, setFeaturedVideos] = useState([]);
   const [newVideos, setNewVideos] = useState([]);
   const [playedVideos, setPlayedVideos] = useState([]);
   const [trendingVideos, setTrendingVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const [articleIds, setArticleIds] = useState<number[]>([]);
   const { showSpotlight, setShowSpotlight } = useSpotlight();
   const timerRef = useRef<number | undefined>();
@@ -65,7 +70,7 @@ const HomePage: React.FC<{
   var getHomeDataTrace: any;
 
   // Get all the props from the modal context
-  const { 
+  const {
     isVideoModalOpen,
     setVideoModalOpen,
     setArticleDetailModalOpen,
@@ -82,14 +87,20 @@ const HomePage: React.FC<{
     // Start loader
     setIsLoading(true);
     try {
-      // Start a Firebase trace      
+      // Start a Firebase trace
       if (tracingEnabled) {
-          getHomeDataTrace = trace(firebasePerf, "getHomeDataTrace");
-          await getHomeDataTrace.start();
+        getHomeDataTrace = trace(firebasePerf, "getHomeDataTrace");
+        await getHomeDataTrace.start();
       }
-      
+
       // Get the data from the API
-      const { featuredVideos, newVideos, playedVideos, trendingVideos, articleIds } = await getHomeDataFromApi();
+      const {
+        featuredVideos,
+        newVideos,
+        playedVideos,
+        trendingVideos,
+        articleIds,
+      } = await getHomeDataFromApi();
 
       setFeaturedVideos(featuredVideos);
       setNewVideos(newVideos);
@@ -97,14 +108,14 @@ const HomePage: React.FC<{
       setTrendingVideos(trendingVideos);
       setArticleIds(articleIds);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     } finally {
       // Hide the splash screen after data has been fetched
       SplashScreen.hide();
       // Stop the trace
       if (tracingEnabled) {
         getHomeDataTrace.stop();
-    }
+      }
     }
   };
 
@@ -112,28 +123,31 @@ const HomePage: React.FC<{
   useEffect(() => {
     // If the API has returned something, and did not return articles, dismiss the loader. Otherwise, the loader will be dismissed in the onArticlesLoaded callback
     if (
-      (featuredVideos.length > 0 || newVideos.length > 0 || playedVideos.length > 0 || trendingVideos.length > 0 || articleIds.length > 0 ) 
-      && 
-      (articleIds.length == 0)) 
-  {
-    setIsLoading(false);
-  }
+      (featuredVideos.length > 0 ||
+        newVideos.length > 0 ||
+        playedVideos.length > 0 ||
+        trendingVideos.length > 0 ||
+        articleIds.length > 0) &&
+      articleIds.length == 0
+    ) {
+      setIsLoading(false);
+    }
   }, [articleIds, trendingVideos, featuredVideos, newVideos, playedVideos]);
 
   // On mount, check if Messages is the currentTab. If so, set it to Home.
-  // This happens when the user navigates to Home from the Messages tab via 
+  // This happens when the user navigates to Home from the Messages tab via
   // the button present before they get their first message
   // Refactor: This is not good code and should be re-written when it makes business sense
   useEffect(() => {
-    if (currentTab === 'Messages') {
-      currentTab = 'Home';
+    if (currentTab === "Messages") {
+      currentTab = "Home";
     }
   }, []);
 
   // This runs on mount and every time currentTab changes or the video modal opens/closes
   // We want to fetch new data when the modal closes because there's a good chance we have new videos to serve
   useEffect(() => {
-    if (currentTab === 'Home') {
+    if (currentTab === "Home") {
       let timeoutId: any;
 
       // Start a timer
@@ -145,16 +159,18 @@ const HomePage: React.FC<{
           logUserFact({
             cadeyUserId: cadeyUserId,
             baseApiUrl: apiUrl,
-            userFactTypeName: 'ErrorLog',
-            appPage: 'App Open',
-            detail1: 'getAppData call (/appopened) took longer than 10 seconds. Time: ' + new Date().toISOString(),
+            userFactTypeName: "ErrorLog",
+            appPage: "App Open",
+            detail1:
+              "getAppData call (/appopened) took longer than 10 seconds. Time: " +
+              new Date().toISOString(),
           });
-          
+
           logErrorToFirestore({
             userID: cadeyUserId,
             timestamp: new Date().toISOString(),
-            error: 'getAppData call (/appopened) took longer than 10 seconds',
-            context: "Fetching App Data"
+            error: "getAppData call (/appopened) took longer than 10 seconds",
+            context: "Fetching App Data",
           });
 
           setIsLoading(false); // Optionally stop the loader
@@ -162,7 +178,7 @@ const HomePage: React.FC<{
       }, 10000); // Set timeout for 10 seconds
 
       fetchData(); // Fetch the homepage data if the user is on the Home tab
-      
+
       if (tutorialStep === 0) {
         setTutorialStep(1); // If the tutorial hasn't started, mark it completed
       }
@@ -176,34 +192,32 @@ const HomePage: React.FC<{
   // Check for onboarding quiz on mount and when cadeyUserId changes
   useEffect(() => {
     const checkOnboarding = async () => {
-      
       const requestQuiz = async () => {
         const quizResponse = await getQuiz(
           apiUrl,
           Number(cadeyUserId),
-          2,                    // Client Context: Where the user is in the app (1 = VideoDetail)
-          0,                    // Entity Type (1 = video)
-          0                     // Entity IDs (The ID of the video)
+          2, // Client Context: Where the user is in the app (1 = VideoDetail)
+          0, // Entity Type (1 = video)
+          0, // Entity IDs (The ID of the video)
         );
-    
-        if (quizResponse.question !== null && quizResponse.question.id > 0) {
 
+        if (quizResponse.question !== null && quizResponse.question.id > 0) {
           // Set the quiz data
           setQuizModalData(quizResponse);
-    
+
           // Show the welcome screen
           console.log("Showing welcome modal");
           setWelcomeModalOpen(true);
 
           // NOTE: We don't open the quiz modal here as the user needs to see the welcome screen first. Welcome screen will open the quiz modal.
         }
-      }
-      
+      };
+
       if (cadeyUserId) {
         requestQuiz();
       }
-    }
-  
+    };
+
     if (cadeyUserId) {
       checkOnboarding();
     }
@@ -214,13 +228,13 @@ const HomePage: React.FC<{
   // useEffect(() => {
   //   // If the tutorial has already progressed, don't set a new timer.
   //   if (tutorialStep !== 0) return;
-    
+
   //   // Wait to show the spotlight if the user has trending videos and the tutorial step is 0
   //   if (trendingVideos.length > 0 && tutorialStep === 0) {
   //     timerRef.current = window.setTimeout(() => {
   //       setShowSpotlight(true);
   //       setTutorialStep(1);
-  //     }, 8000); 
+  //     }, 8000);
   //   }
 
   //   // This is to ensure if for some reason the component gets unmounted, we clear the timer
@@ -231,31 +245,30 @@ const HomePage: React.FC<{
 
   // On component mount:
   useEffect(() => {
-    document.title = 'Home'; // Set the page title
-    setCurrentBasePage('Home'); // Set the current base page
-    setCurrentAppPage('Home'); // Set the current app page
+    document.title = "Home"; // Set the page title
+    setCurrentBasePage("Home"); // Set the current base page
+    setCurrentAppPage("Home"); // Set the current app page
     logUserFact({
       cadeyUserId: cadeyUserId,
       baseApiUrl: apiUrl,
-      userFactTypeName: 'appPageNavigation',
-      appPage: 'Home',
+      userFactTypeName: "appPageNavigation",
+      appPage: "Home",
     });
 
     // Dismiss the spotlight on interaction
     const handleInteraction = (event: MouseEvent | TouchEvent) => {
       setShowSpotlight(false);
-    };  
-      
-    // Add event listeners for clicks and touches, and handle the interaction
-    document.addEventListener('click', handleInteraction);
-    document.addEventListener('touchstart', handleInteraction);
-  
-    return () => {
-      document.removeEventListener('click', handleInteraction);
-      document.removeEventListener('touchstart', handleInteraction);
     };
-  }
-  , []);
+
+    // Add event listeners for clicks and touches, and handle the interaction
+    document.addEventListener("click", handleInteraction);
+    document.addEventListener("touchstart", handleInteraction);
+
+    return () => {
+      document.removeEventListener("click", handleInteraction);
+      document.removeEventListener("touchstart", handleInteraction);
+    };
+  }, []);
 
   // Show the modal if a vimeoId is passed in via query string
   useEffect(() => {
@@ -286,34 +299,33 @@ const HomePage: React.FC<{
     // Restrict input to 100 characters
     const inputValue = e.detail.value;
     if (inputValue.length > 100) {
-        const limitedValue = inputValue.slice(0, 100);
-        e.target.value = limitedValue;
+      const limitedValue = inputValue.slice(0, 100);
+      e.target.value = limitedValue;
     }
-}
+  };
 
-const handleSearchInput = async (e: React.KeyboardEvent) => {
-  // Dismiss the spotlight
-  setShowSpotlight(false);
-  if (timerRef.current) clearTimeout(timerRef.current);
-  
-  const searchTerm = (e.target as HTMLInputElement).value;
-    
+  const handleSearchInput = async (e: React.KeyboardEvent) => {
+    // Dismiss the spotlight
+    setShowSpotlight(false);
+    if (timerRef.current) clearTimeout(timerRef.current);
+
+    const searchTerm = (e.target as HTMLInputElement).value;
+
     if (e.key === "Enter") {
+      // Check if the user has entered a search term
+      if (searchTerm.trim() === "") {
+        alert("Please enter a search term.");
+        return;
+      }
 
-        // Check if the user has entered a search term
-        if (searchTerm.trim() === "") {
-            alert("Please enter a search term.");
-            return;
-        }
-
-        // Route the user to the search page
-        history.push({
-          pathname: '/App/Search',
-          search: `?query=${encodeURIComponent(searchTerm)}`, // Optional if you want the term in the URL
-          state: { query: searchTerm }
-        });
+      // Route the user to the search page
+      history.push({
+        pathname: "/App/Search",
+        search: `?query=${encodeURIComponent(searchTerm)}`, // Optional if you want the term in the URL
+        state: { query: searchTerm },
+      });
     }
-}
+  };
 
   return (
     <IonPage className="home">
@@ -323,36 +335,36 @@ const handleSearchInput = async (e: React.KeyboardEvent) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
         {/* Show a loading state if necessary */}
         {isLoading && (
-          <IonLoading isOpen={true} message={'Loading your data...'} />
+          <IonLoading isOpen={true} message={"Loading your data..."} />
         )}
 
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">{trendingVideos.length > 0 ? "Welcome" : "Home"}</IonTitle>
+            <IonTitle size="large">
+              {trendingVideos.length > 0 ? "Welcome" : "Home"}
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
-        
+
         {!trendingVideos.length && (
           <IonRow className="search-container">
             {/* Search bar */}
-            <IonSearchbar 
-                className="search-bar" 
-                onIonChange={handleInputChange}
-                onKeyDown={handleSearchInput}
-                mode="ios"
+            <IonSearchbar
+              className="search-bar"
+              onIonChange={handleInputChange}
+              onKeyDown={handleSearchInput}
+              mode="ios"
             ></IonSearchbar>
           </IonRow>
         )}
-        
+
         <IonRow>
           <IonText className="subcopy">
-            {trendingVideos.length > 0 
+            {trendingVideos.length > 0
               ? "What brings you here today?"
-              : "Here are a few suggestions, based on your concerns about your child."
-            }
+              : "Here are a few suggestions, based on your concerns about your child."}
           </IonText>
         </IonRow>
         <hr className="divider" />
@@ -369,36 +381,39 @@ const handleSearchInput = async (e: React.KeyboardEvent) => {
             {/* If user has watched videos, show this. Else, skip it */}
             {playedVideos.length > 0 && (
               <IonRow className="video-list-row recently-viewed">
-                  <h2>Recently Viewed</h2>
-                  <VideoList videos={playedVideos} listType='horizontal' />
+                <h2>Recently Viewed</h2>
+                <VideoList videos={playedVideos} listType="horizontal" />
               </IonRow>
             )}
             {/* If user has featured videos, show this. Else, skip it */}
             {featuredVideos.length > 0 && (
               <IonRow className="video-list-row featured">
-                  <h2>Watch Now</h2>
-                  <VideoList videos={featuredVideos} listType='horizontal' /> 
+                <h2>Watch Now</h2>
+                <VideoList videos={featuredVideos} listType="horizontal" />
               </IonRow>
             )}
             {/* If user has articles, show this. Else, skip it */}
             {articleIds.length > 0 && (
               <IonRow className="article-list-row">
                 <h2>Read Now</h2>
-                <ArticlesListHorizontal articleIds={articleIds} onLoaded={onArticlesLoaded} />
+                <ArticlesListHorizontal
+                  articleIds={articleIds}
+                  onLoaded={onArticlesLoaded}
+                />
               </IonRow>
             )}
             {/* If user has new videos, show this. Else, skip it */}
             {newVideos.length > 0 && (
               <IonRow className="video-list-row new">
-                  <h2>New Videos</h2>
-                  <VideoList videos={newVideos} listType='horizontal' />
+                <h2>New Videos</h2>
+                <VideoList videos={newVideos} listType="horizontal" />
               </IonRow>
             )}
             {/* If user has trending videos, show this. Else, skip it */}
             {trendingVideos.length > 0 && (
               <IonRow className="video-list-row trending">
-                  <h2>Trending Now</h2>
-                  <VideoList videos={trendingVideos} listType='horizontal' />
+                <h2>Trending Now</h2>
+                <VideoList videos={trendingVideos} listType="horizontal" />
               </IonRow>
             )}
           </>
