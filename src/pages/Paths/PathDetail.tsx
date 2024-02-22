a0b01481506b0894956f61e4d588bf98efe67921
bounddigital <austin@bounddigital.com>
2024-02-22 16:22:57 -0700

    Commenting out share instead of removing completely

--- src/pages/Paths/PathDetail.tsx
+++ src/pages/Paths/PathDetail.tsx
@@ -348,6 +348,19 @@ const PathDetailPage: React.FC<PathDetailModalProps> = () => {
                                 onVideoEnd={handleVideoEnd}
                                 on75PercentProgress={handle75PercentProgress}
                             />
+                            {/* <div className="video-metadata" style={{ marginTop: videoHeight || 0 }}>
+                                <div className="tag-share">
+                                    {canShare && pathEntity.sourceId && (
+                                        <div className="share" onClick={(event) => handleShare(event, pathEntity.sourceId, pathEntity.entityId.toString())}>
+                                            <p>Share </p>
+                                            <div className="share-button">
+                                                <IonIcon icon={arrowRedoOutline} />
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                                <h3>{pathEntity.title}</h3>
+                            </div> */}
                         </div>
                     </IonRow>
                 )}
