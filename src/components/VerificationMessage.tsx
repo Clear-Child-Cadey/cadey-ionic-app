95d612e0d791d3d8ee2bec820ebddd9d545cef88
Brandenvdw <brandenvdw2616@gmail.com>
2024-03-19 16:59:51 +0200

    feat: update verification message copy as well

--- src/components/VerificationMessage.tsx
+++ src/components/VerificationMessage.tsx
@@ -1,13 +1,14 @@
 import { IonButton } from '@ionic/react';
 import './VerificationMessage.css';
+import AppMeta from '../variables/AppMeta';
 interface Props {
   handleRedirectHome: () => void;
 }
 const VerificationPage = ({ handleRedirectHome }: Props) => {
   return (
     <div className='email-verification-message'>
-      <h2>UH-OH!</h2>
-      <p>Before you can continue, please verify your email address</p>
+      <h2>Great! Now, check your email</h2>
+      <p>{AppMeta.emailVerificationMessage}</p>
       <IonButton onClick={handleRedirectHome}>I already verified</IonButton>
     </div>
   );
