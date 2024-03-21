3e3aae24348cb1ff85c990145d31d74f677a425a
Mat Gargano <mgargano@gmail.com>
2024-03-21 13:00:25 -0400

    set forceEmailVerification to true

--- src/variables/AppMeta.ts
+++ src/variables/AppMeta.ts
@@ -3,7 +3,7 @@
 //MG this should be refactored into env variables
 
 const AppMeta = {
-  forceEmailVerification: false,
+  forceEmailVerification: true,
   firestoreCollection: 'ErrorLogs2024',
   cadeyApiKey: 'XPRt31RRnMb7QNqyC5JfTZjAUTtWFkYU5zKYJ3Ck',
   fetchTimeout: -1,
