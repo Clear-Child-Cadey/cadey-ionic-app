f06ec038d4cc737b94aedf1bf725576cb3a74e7e
bounddigital <austin@bounddigital.com>
2024-03-29 14:57:14 -0600

    UI work for Grandfather flow

--- src/pages/Grandfather/Grandfather.tsx
+++ src/pages/Grandfather/Grandfather.tsx
@@ -17,28 +17,32 @@ import './Grandfather.css';
 const GrandfatherPage = () => {
   const history = useHistory();
 
-  const handleButtonClick = () => {
+  const handleContinue = (url: string) => {
     // Log user fact?
 
-    history.push('/App/Authentication/Register');
+    history.push(url);
   };
 
   return (
-    <IonPage className='grandfather-page'>
-      <IonHeader class='header'>
-        <IonToolbar className='header-toolbar'>
-          <h2>Grandfather!</h2>
-        </IonToolbar>
-      </IonHeader>
+    <IonPage className='welcome grandfather'>
       <IonContent fullscreen>
-        <IonRow>
-          <p className='grandfather-copy'>
-            We're grandfathering your account permanently! Please register to
-            continue using Cadey for free.
-          </p>
-          <IonButton className='grandfather-button' onClick={handleButtonClick}>
-            Register
-          </IonButton>
+        <IonRow className='welcome-content'>
+          <h2>Welcome back to Cadey!</h2>
+          <IonRow className='grandfather-text'>
+            We have made some enhancements. To continue using the app, please
+            create an account. It's quick and easy!
+          </IonRow>
+          <IonRow className='continue-row'>
+            <IonButton
+              className='continue-button'
+              onClick={() => handleContinue('/App/Authentication/Register')}
+            >
+              Signup
+            </IonButton>
+          </IonRow>
+        </IonRow>
+        <IonRow className='welcome-image'>
+          <img src='assets/images/welcome.png' />
         </IonRow>
       </IonContent>
     </IonPage>
